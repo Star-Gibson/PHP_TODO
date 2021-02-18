2a6d372cf6f643b7777276c0e4d2344fdda66bd4
Star-Gibson <sgibsoncoding@gmail.com>
2021-02-18 14:58:14 -0500

    Created Layout, linked CSS/JS via mix, tested current functionality

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <!-- SCRIPTS -->
+  <script src="{{mix('js/app.js')}}" defer></script>
+
+  <!-- STYLES -->
+  <link href="{{ mix('css/app.css')}}" rel="stylesheet">
+
+  <title>WolfPack Todo's</title>
+</head>
+
+<body>
+  @yield('content')
+</body>
+
+</html>
