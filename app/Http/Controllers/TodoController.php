6ad1003989677d9ff2122f092cd35474eb18579f
Star-Gibson <sgibsoncoding@gmail.com>
2021-02-18 19:34:47 -0500

    Added Input Field, adjusted TodoConroller and routes

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -14,7 +14,7 @@ class TodoController extends Controller
      */
     public function index()
     {
-        //
+        return view('welcome');
     }
 
     /**
@@ -24,7 +24,7 @@ class TodoController extends Controller
      */
     public function create()
     {
-        //
+        
     }
 
     /**
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,4 +2,20 @@
 
 @section('content')
 
+<div class="w-100 h-100 d-flex justify-content-center align-items-center">
+    <div class="text-center" style="width: 40%">
+        <h1 class="display-4 text-white">Wolfpack Todo's</h1>
+        <!-- SAVING INPUT INTO DATABASE -->
+        <form action="{{route('todo.store')}}" method="POST">
+            <div class="input-group mb-3 w-100">
+                <input type="text" class="form-control form-control-lg" name="title" placeholder="What do you need todo?">
+                <div class="input-group-append">
+                    <!-- CHANGE TO BUTTON TO PLUS SIGN -->
+                    <button class="btn btn-danger" type="submit" id="button-addon2">Add</button>
+                </div>
+            </div>
+        </form>
+    </div>
+</div>
+
 @endsection
