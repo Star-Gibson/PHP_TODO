d282a60a7c1f8916bdfedb542e6ae04dbf17b0d7
Star-Gibson <sgibsoncoding@gmail.com>
2021-02-20 15:35:54 -0500

    Changed icon for in-progress todos, added delete icon

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -24,14 +24,14 @@
                 <div class='p-2'>
                     @if($todo->completed === 0)
                     <!-- Icon -->
-                    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-square-x" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ff2825" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-refresh" width="30px" height="30px" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ff2825" fill="none" stroke-linecap="round" stroke-linejoin="round">
                         <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                        <rect x="4" y="4" width="16" height="16" rx="2" />
-                        <path d="M10 10l4 4m0 -4l-4 4" />
+                        <path d="M20 11a8.1 8.1 0 0 0 -15.5 -2m-.5 -4v4h4" />
+                        <path d="M4 13a8.1 8.1 0 0 0 15.5 2m.5 4v-4h-4" />
                     </svg>
                     @else
                     <!-- ICON -->
-                    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-square-check" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#00b341" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-square-check" width="30px" height="30px" viewBox="0 0 24 24" stroke-width="1.5" stroke="#00b341" fill="none" stroke-linecap="round" stroke-linejoin="round">
                         <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                         <rect x="4" y="4" width="16" height="16" rx="2" />
                         <path d="M9 12l2 2l4 -4" />
@@ -46,7 +46,7 @@
                         @method('PUT')
                         @csrf
                         <input type="text" value="1" name="completed" hidden>
-                        <button class="btn btn-success">Complete</button>
+                        <button class="btn btn-dark">Complete</button>
                     </form>
                     @else
                     <form action="{{route('todo.update', $todo->id)}}" method="POST">
@@ -58,11 +58,31 @@
                     </form>
                     @endif
                     <form action="{{route('todo.destroy', $todo->id)}}" method="POST">
-                    @method('DELETE')
-                    @csrf
-                    <button class="btn btn-danger">DELETE</button>
-                    <!-- DESTROY -->
+                        <!-- ALLOWS FORM TO USE DELETE METHOD -->
+                        @method('DELETE')
+                        @csrf
+                        <button class="btn">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-trash" width="30px" height="30px" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ff2825" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                <line x1="4" y1="7" x2="20" y2="7" />
+                                <line x1="10" y1="11" x2="10" y2="17" />
+                                <line x1="14" y1="11" x2="14" y2="17" />
+                                <path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" />
+                                <path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" />
+                            </svg>
+                        </button>
                     </form>
+
+
+
+
+                    <!-- PENCIL -->
+                    <!-- <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-pencil" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#000000" fill="none" stroke-linecap="round" stroke-linejoin="round">
+  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+  <path d="M4 20h4l10.5 -10.5a1.5 1.5 0 0 0 -4 -4l-10.5 10.5v4" />
+  <line x1="13.5" y1="6.5" x2="17.5" y2="10.5" />
+</svg> -->
+
 
 
                 </div>
